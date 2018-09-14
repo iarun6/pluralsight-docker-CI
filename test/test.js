6856a4b9ab6ee043cfad1c610e8815cc11dfe3d5
arun_d3v <iarunrmenon@gmail.com>
2018-09-14 13:28:44 +0530

    commit with latest changes for test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
-// // Simple async test for HTTP 200 response code using supertest
-// 'use strict';
+// Simple async test for HTTP 200 response code using supertest
+'use strict';
 
-// var request = require("supertest"),
-//     app = require("../app").getApp;
+var request = require("supertest"),
+    app = require("../app").getApp;
 
-// describe('GET /', function(){
-//   it('expects HTTP response 200', function(done){
-//     request(app)
-//      .get('/')
-// 	 .expect(200, done); 
-//   });
-// });
+describe('GET /', function(){
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
+});
 
-console.log("Hello, world!");
+// console.log("Hello, world!");
